baddd3f58285339d6a2d41e2231688fe705468ff
theB264 <public88878878887@hotmail.com>
2014-05-15 01:08:37 -0500

    initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GgMrp::Application.config.secret_token = 'bcf66de5977812bce47dd439a8c332a4282968915196d7b13aac0ecfc87028098dadb70a66a6046b9bb88d362f35ddefefc9e1c8152125704bc9262471a73fc7'
