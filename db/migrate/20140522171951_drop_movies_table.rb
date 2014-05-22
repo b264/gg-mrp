eefb0b9f6763d426d3bf401ca4555702fc22548c
theB264 <public88878878887@hotmail.com>
2014-05-22 16:58:13 -0500

    error on migrate

--- db/migrate/20140522171951_drop_movies_table.rb
+++ db/migrate/20140522171951_drop_movies_table.rb
@@ -1,6 +1,6 @@
 class DropMoviesTable < ActiveRecord::Migration
   def up
-    drop_table :movies
+    #drop_table :movies
   end
   def down
     raise ActiveRecord::IrreversibleMigration, "You cannot reverse this migration 'down' because that would create an unused table which was dropped already."
